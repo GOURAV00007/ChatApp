002c1c5dbc3c47d3c87c5f0566d3d8335ef130cc
GOURAV00007 <145568819+GOURAV00007@users.noreply.github.com>
2025-06-22 13:51:07 +0530

    Update Search.jsx

--- FrontEnd/src/components/Search/Search.jsx
+++ FrontEnd/src/components/Search/Search.jsx
@@ -16,7 +16,7 @@ function Search() {
       })
       .then((res) => {
         setUserData(res.data.data);
-        alert(res.data.success);
+        // alert(res.data.success);
       })
       .catch((err) => {
         console.log(err);
